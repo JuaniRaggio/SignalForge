ad3409915a886f8833e4564a3bb17f228eff2e1d
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-14 16:57:47 -0300

    feat(backend): init models

--- src/signalforge/models/__init__.py
+++ src/signalforge/models/__init__.py
@@ -1,1 +1,15 @@
 """SQLAlchemy models."""
+
+from signalforge.models.base import Base, TimestampMixin
+from signalforge.models.news import NewsArticle
+from signalforge.models.price import Price
+from signalforge.models.user import User, UserType
+
+__all__ = [
+    "Base",
+    "TimestampMixin",
+    "NewsArticle",
+    "Price",
+    "User",
+    "UserType",
+]
