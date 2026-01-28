7145b573bc7170ec7c7e701d55d31ddf1231335e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-28 17:03:33 -0300

    chore(config): shap version configuration

--- src/signalforge/ml/__init__.py
+++ src/signalforge/ml/__init__.py
@@ -4,4 +4,5 @@ This module provides ML capabilities including:
 - Feature engineering for technical indicators
 - Model training and evaluation
 - Prediction pipelines
+- Model explainability with SHAP
 """
